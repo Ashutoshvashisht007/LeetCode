3fa41128e618bdab27291612e81c7995631d9f20
Ashutoshvashisht007 <85727351+Ashutoshvashisht007@users.noreply.github.com>
2024-08-05 10:43:35 +0530

    Time: 11 ms (90.72%) | Memory: 18.6 MB (14.46%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
-        unordered_map<string,pair<int,int>> mp;
+        unordered_map<string,int> mp;
 
-        for(int i = 0; i < arr.size(); i++){
-            if(mp.find(arr[i]) != mp.end())
-            {
-                int temp = mp[arr[i]].first;
-                mp[arr[i]] = {temp+1,i};
-            }
-            else
-                mp[arr[i]] = {1,i};
+        for(auto &it : arr){
+            mp[it]++;
         }
-        priority_queue<pair<int,string>,vector<pair<int,string>>,greater<pair<int,string>>> pq;
-
-        for(auto &it : mp)
+        for(auto &it : arr)
         {
-            if(it.second.first == 1)
-            {
-                pq.push({it.second.second,it.first});
-            }
-        }
-
-        while(!pq.empty() && k > 0){
-            auto it = pq.top();
-            pq.pop();
-            k--;
-            if(k == 0){
-                return it.second;
+            if(mp[it] == 1){
+                k--;
+                if(!k){
+                    return it;
+                }
             }
         }
 
